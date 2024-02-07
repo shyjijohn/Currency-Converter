3933dc65eadb4185fe317edefaf29b6b03f329d9
Shyji John <shyjijohn90@gmail.com>
2024-02-07 14:35:26 +0000

    UI of currency converter

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div class="container"><h1>Currency Converter</h1></div>
+      <div class="row">
+      <div class="col">
+      <select>
+        <option>select</option>
+      </select>
+      <input type="text"></input>
+      </div>
+
+      <div class="col">
+      <select>
+        <option>select</option>
+      </select>
+      <input type="text"></input>
+      </div>
+      </div>
+
+      <button type="submit">Convert</button>
+    </div>
+  );
+}
+
+export default App;

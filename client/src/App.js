81ae4b81df4e4517737af87d3e4815864ef516ae
Shyji John <shyjijohn90@gmail.com>
2024-03-09 20:12:43 +0000

    accessed the historical rates of the dates from the api

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
+import { DateRangePicker } from 'react-date-range';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
+
 
 
 function App() {
@@ -106,7 +110,7 @@ function App() {
   }
   console.log("Returning APP")
 
-  // box2
+  // historical
 
   const [selectedDate, setSelectedDate] = useState(null);
   const [amount, setAmount] = useState(null);
@@ -118,20 +122,28 @@ function App() {
 
   useEffect(() => {
     console.log("useEffect running...");
-  }, [{ selectedDate }])
+  }, [selectedDate])
 
-  const handleDateChange = (date) => {
+  const handleDateFormat = (date) => {
     // setSelectedDate(date);
     console.log("setSelectedDate1" + date)
 
+    console.log("Now its Combined of Date and Time...." + date);
     var year = date.getFullYear();
+    console.log("Now its year....", year);
+
     var month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
     var day = String(date.getDate()).padStart(2, '0');
     var dateChosen = `${year}-${month}-${day}`;
 
-    setSelectedDate(dateChosen);
-    console.log("setSelectedDate2" + dateChosen)
+    return dateChosen
 
+  };
+
+  const handleDateChange = (date) => {
+    const datePickerDate = handleDateFormat(date);
+    setSelectedDate(datePickerDate);
+    console.log("setSelectedDate2" + selectedDate)
   };
 
   const handleCollect = (() => {
@@ -155,7 +167,6 @@ function App() {
 
       setAmount(valuesOfObject[0])
       console.log("amount", valuesOfObject[0])
-
       setBase(valuesOfObject[1])
       console.log("base", valuesOfObject[1])
 
@@ -164,10 +175,74 @@ function App() {
       setExchangeRates(dataRates)
 
       setResultDateStr(valuesOfObject[2])
-    }
-    )
-
+    })
   })
+
+  // date range  
+
+  const [selectionRange, setSelectionRange] = useState({
+    startDate: new Date(),
+    endDate: new Date(),
+    key: 'selection',
+  });
+
+  const [startDateInList, setStartDateInList] = useState('')
+  const [endDateInList, setEndDateInList] = useState('')
+  const [historicalAmount, setHistoricalAmount] = useState();
+  const [historicalBase, setHistoricalBase] = useState();
+  const [historicalRates, setHistoricalRates] = useState();
+  const [isHistoricalRates, setIsHistoricalRates] = useState(false);
+
+  // console.log("setSelectionRangeCom",   {selectionRange}  );
+  // console.log("setSelectionRangeCom",   {selectionRange}  );
+
+  useEffect(() => {
+    console.log("useEffect running for dateRange...");
+  }, [startDateInList])
+  useEffect(() => {
+    console.log("useEffect running for dateRange...");
+  }, [endDateInList])
+
+  const handleSelect = (ranges) => {
+    setSelectionRange(ranges.selection);
+    console.log("setSelectionRange", ranges.selection);
+
+    const startDateRangePicker = handleDateFormat(ranges.selection.startDate);
+    setStartDateInList(startDateRangePicker);
+    console.log("startDateInList....1...." + startDateRangePicker)
+
+    const endDateRangePicker = handleDateFormat(ranges.selection.endDate);
+    setEndDateInList(endDateRangePicker);
+    console.log("endDateInList....1...." + endDateRangePicker)
+  };
+
+  const historicalRatesForDatesGiven = () => {
+    var fetchingDateRange = fetch(`https://api.frankfurter.app/${startDateInList}..${endDateInList}`)
+    var fetchingDateRangeJson = fetchingDateRange.then((data) => data.json())
+    fetchingDateRangeJson.then((data) => {
+      console.log("jsondata for dataRange.....", data)
+      const valuesOfdateRange = Object.values(data)
+      console.log("valuesOfdateRange", valuesOfdateRange)
+
+      const amountForDateRange = valuesOfdateRange[0]
+      setHistoricalAmount(amountForDateRange)
+      console.log("amountForDateRange", amountForDateRange)
+
+      const baseForDateRange = valuesOfdateRange[1]
+      setHistoricalBase(baseForDateRange)
+      console.log("baseForDateRange", baseForDateRange)
+
+      const historicalRatesForDateRange = Object.entries(valuesOfdateRange[4])
+      setHistoricalRates(historicalRatesForDateRange)
+      console.log("historicalRatesForDateRange", historicalRatesForDateRange)
+
+      setIsHistoricalRates(!isHistoricalRates)
+    })
+  }
+
+
+
+
   return (
     //make a box with shadow effect
     //set the heading convert and charts
@@ -248,7 +323,7 @@ function App() {
               />
               <button onClick={handleCollect}>Collect</button>
             </div>
-            <div className='infoText'>RESULT</div>
+            <div className='infoText'><h4>RESULT</h4></div>
             <div className='infoText'>Amount: {amount}</div>
             <div className='infoText' >Base: {base}</div>
             <div className='infoText'>Date: {resultDateStr}</div>
@@ -262,10 +337,63 @@ function App() {
               </ul>
             </div>
           </div>
-        </div>
 
+          <div class="dateRanges">
+
+            <DateRangePicker
+              ranges={[selectionRange]}
+              onChange={handleSelect}
+            />
+            <div class="dateRangesList">
+              Dates between {startDateInList} and {endDateInList}
+
+              <button onClick={historicalRatesForDatesGiven}>Historical Rates</button>
+              {isHistoricalRates && (
+                <>
+                  <div className='infoText'><h4>RESULT</h4></div>
+                  <div className='infoText'>Amount: {historicalAmount}</div>
+                  <div className='infoText' >Base: {historicalBase}</div>
+                  <div className='infoTextOfRates'>Rates:
+                    <div className='multiResult'>
+                      {
+                        historicalRates.map(([key, value]) => {
+                          // console.log("Valuefrom map ", value)
+                          return (
+                            // <li key={key}>
+                            <div >
+                              <strong>{key}:</strong>
+                              {
+                                Object.entries(value).map(([keyOfObj, valueOfObj]) => {
+                                  // console.log("keyOfObj: ", keyOfObj);
+                                  // console.log("valueOfObj: ", valueOfObj);
+                                  return (
+                                    // <li key={keyOfObj}>
+                                    <div className='eachDayResult'>
+
+                                      <div>{keyOfObj}:{valueOfObj}</div>
+                                      <br />
+                                    </div>
+
+                                    // </li>
+                                  )
+                                })
+                              }
+                            </div>
+                            // </li>
+                          )
+                        })}
+                    </div>
+                  </div>
+                </>
+              )}
+
+            </div>
+
+          </div>
+        </div>
       </div>
-    </div>
+
+    </div >
   );
 }
 

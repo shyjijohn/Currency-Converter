fd1e377c8eb454205ee60888d303b1305dc7a999
Shyji John <shyjijohn90@gmail.com>
2026-07-17 00:22:17 +0100

    routing issue fixed

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css'
 // import AppContext from './AppContext';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <BrowserRouter >
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
   // </React.StrictMode>
 );
 
